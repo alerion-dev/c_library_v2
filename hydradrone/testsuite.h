0a3363c833bd50d050a917578abb8ae0baf47929
Antoine RICHARD <antoine.richard@alerion.fr>
2020-04-01 15:32:02 +0200

    Rebased on ardupilotmega for QGC

--- hydradrone/testsuite.h
+++ hydradrone/testsuite.h
@@ -13,16 +13,25 @@ extern "C" {
 #ifndef MAVLINK_TEST_ALL
 #define MAVLINK_TEST_ALL
 static void mavlink_test_common(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_uAvionix(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_icarous(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_ardupilotmega(uint8_t, uint8_t, mavlink_message_t *last_msg);
 static void mavlink_test_hydradrone(uint8_t, uint8_t, mavlink_message_t *last_msg);
 
 static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_common(system_id, component_id, last_msg);
+    mavlink_test_uAvionix(system_id, component_id, last_msg);
+    mavlink_test_icarous(system_id, component_id, last_msg);
+    mavlink_test_ardupilotmega(system_id, component_id, last_msg);
     mavlink_test_hydradrone(system_id, component_id, last_msg);
 }
 #endif
 
 #include "../common/testsuite.h"
+#include "../uAvionix/testsuite.h"
+#include "../icarous/testsuite.h"
+#include "../ardupilotmega/testsuite.h"
 
 
 static void mavlink_test_hydradrone_status(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
